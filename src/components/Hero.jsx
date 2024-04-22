b1620860958f5596c0825c74307ffac1aff8e1bd
Assaf1er <assaffart@gmail.com>
2024-04-22 14:02:04 +0100

    added profile picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,11 @@
 import { Image } from "@rasenganjs/image";
-import profile from "@assets/beral-profil.png";
+import profile from "@assets/profile.png";
 
 function Hero() {
   return (
     <section className=" text-primary bg-gray w-full h-screen  flex flex-col justify-center py-8 px-[20px] md:px-[50px] xl:px-[100px] font-ojuju">
       <div className="font-normal">
-        {" "}
         <p className="text-[50px]">
-          {" "}
           Hi, i'm <span className="font-bold">Beral ASSONFACK</span>
         </p>
         <p className="text-[50px]">
@@ -16,7 +14,7 @@ function Hero() {
         <p className="text-[25px] ">Web Developper / Frontend specialist</p>
       </div>
       <div>
-        <Image src={profile} alt="" width={200} height={200} />
+        <Image src={profile} alt="profile picture" width={200} height={200} />
       </div>
     </section>
   );
