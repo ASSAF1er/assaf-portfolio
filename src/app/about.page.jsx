de2a612f1cebcc7438bb4650d0b26fa2177d9c8e
Assaf1er <assaffart@gmail.com>
2024-05-15 23:59:02 +0100

    responsive about page

--- src/app/about.page.jsx
+++ src/app/about.page.jsx
@@ -35,6 +35,25 @@ const About = () => {
   ];
   const galery = [profile1, profile2, profile3, profile4];
 
+  const experiences = [
+    {
+      position: "Frontend Developer - Intern",
+      date: "April 2024 - Present",
+      enterpriseName: "FremoPay",
+      enterpriseUrlrl: "",
+      enterpriseLocation: "Yaoundé, Cameroon",
+      stack: ["ReactJS", " TypeScript ", "Redux ", "TailwindCSS"],
+    },
+    {
+      position: "Web Developer - Intern",
+      enterpriseName: "SEED Soft Engine",
+      date: "Jully 2023 - September 2023",
+      enterpriseUrlrl: "",
+      enterpriseLocation: " Yaoundé Cameroon",
+      stack: ["ReactJS", "NodeJS", "Express", "MongoDB"],
+    },
+  ];
+
   // const items = document?.querySelectorAll(".about-image");
 
   // items.forEach((item) => {
@@ -61,26 +80,31 @@ const About = () => {
       </div>
       {/*presentation */}
       <section className="flex flex-col py-10 md:py-32 w-full h-auto ">
-        <h3 className="font-bold text-[40px] mdtext-[60px] ">I am,</h3>
-        <div className="flex flex-col md:flex-row items-center gap-5 h-full">
-          <div className="flex flex-1 font-medium text-[18px] md:text-[20px] ">
-            Beral ASSONFACK, a web developer passionate about crafting beautiful
-            and flawless websites. With a keen eye for detail and a commitment
-            to precision, I specialize in creating designs that are not only
-            visually stunning but also user-friendly. My passion for soft
-            solutions began when I was still a kid. Bringing out abstract ideas
-            to digital realities through lines of code has always been full of
-            emotion.
+        <div className="flex flex-col-reverse md:flex-row  gap-5 h-full">
+          <div className="flex flex-col flex-1 font-medium text-[18px] md:text-[20px] ">
+            <h3 className="font-bold text-[40px] md:text-[60px] ">I am,</h3>
+            <p>
+              Beral ASSONFACK, a web developer passionate about crafting
+              beautiful and flawless websites. With a keen eye for detail and a
+              commitment to precision, I specialize in creating designs that are
+              not only visually stunning but also user-friendly. My passion for
+              soft solutions began when I was still a kid. Bringing out abstract
+              ideas to digital realities through lines of code has always been
+              full of emotion.
+            </p>
+
             <br />
-            <br />
-            From front-end development using HTML, CSS, JavaScript, React,
-            Tailwind and recently RasenganJS to back-end development with
-            frameworks like Node.js and databases like MongoDB, I thrive on the
-            challenges of building robust and scalable web applications.
+            <p>
+              From front-end development using HTML, CSS, JavaScript, React,
+              Tailwind and recently RasenganJS to back-end development with
+              frameworks like Node.js and databases like MongoDB, I thrive on
+              the challenges of building robust and scalable web applications.
+            </p>
           </div>
-          <div className="group flex h-[250px] md:h-[350px] gap-2 ">
-            {galery?.map((item) => (
+          <div className="group flex justify-center w-full md:w-auto h-[250px] md:h-[350px] md:mt-5 gap-2 ">
+            {galery?.map((item, index) => (
               <img
+                key={index}
                 src={item}
                 alt="profile"
                 className="about-image h-full object-cover grayscale rounded-xl hover:w-[200px] md:hover:w-[300px] w-10   duration-500 ease-in-out"
@@ -95,13 +119,13 @@ const About = () => {
       >
         <h1 className="font-bold text-[40px] text-left ">Skills</h1>
         <div className=" text-[30px] underline mt-5">Frontend</div>
-        <div className="flex gap-1 flex-wrap ">
+        <div className="flex justify-center md:justify-normal gap-1 flex-wrap ">
           {skillsFrontend?.map((item, index) => (
             <SkillCard key={index} logo={item.photo} name={item.name} />
           ))}
         </div>
         <div className="text-[25px] underline mt-5">Backend</div>
-        <div className="flex gap-2 flex-wrap ">
+        <div className="flex justify-center md:justify-normal gap-2 flex-wrap ">
           {skillsBackend?.map((item, index) => (
             <SkillCard key={index} logo={item.photo} name={item.name} />
           ))}
@@ -109,7 +133,7 @@ const About = () => {
         <div className=" text-[25px] underline mt-5">
           Deployment & Version Control
         </div>
-        <div className="flex gap-2 flex-wrap ">
+        <div className="flex justify-center md:justify-normal gap-2 flex-wrap ">
           {skillsDeployment?.map((item, index) => (
             <SkillCard key={index} logo={item.photo} name={item.name} />
           ))}
@@ -122,46 +146,34 @@ const About = () => {
               <div className="absolute top-0 text-cover w-full bg-gray "></div>
             </h1>
             <div className="min-h-2 title-underline w-[200px] bg-primary"></div>
-            <div className="mt-10 group flex flex-col w-full cursor-pointer">
-              <h2 className="text-[20px] md:text-[30px]  ">
-                Frontend Developper - Intern
-              </h2>
-              <div className="flex justify-between font-medium text-[14AZpx] md:text-[20px] ">
-                <h3>
-                  <span className=" no-underline group-hover:underline font-medium hover:font-bold  duration-500">
-                    FreemoPay
-                  </span>
-                  , Yaoundé Cameroon
-                </h3>{" "}
-                <h3 className="text-[14px] md:text-[17px] ">
-                  April 2024 - Present
+            {experiences.map((item, index) => (
+              <div
+                key={index}
+                className="mt-10 group flex flex-col w-full cursor-pointer"
+              >
+                <h2 className="text-[24px] md:text-[30px]  ">
+                  {item.position}
+                </h2>
+                <div className="flex flex-col md:flex-row gap-1 justify-between font-medium text-[18px] md:text-[20px] ">
+                  <h3 className="">
+                    <span className="underline font-medium hover:font-bold  duration-500">
+                      {item.enterpriseName}
+                    </span>
+                    , {item.enterpriseLocation}
+                  </h3>{" "}
+                  <h3 className="text-[15px] md:text-[17px] ">{item.date}</h3>
+                </div>
+                <h3 className="font-medium text-[15px] mt-5">
+                  {item.stack?.map(
+                    (stk, index) =>
+                      `${stk} ${index === item.stack.length - 1 ? "" : " - "}`,
+                  )}
                 </h3>
+                <div className="flex justify-center">
+                  <span className="w-full show-border mt-5 h-0.5 bg-gradient-to-r from-gray via-primary to-gray "></span>
+                </div>
               </div>
-              <h3 className="font-medium text-[15px] mt-5">
-                ReactJS - TypeScript - Redux - TailwindCSS{" "}
-              </h3>
-              <div className="flex justify-center">
-                <span className="w-full show-border mt-5 h-0.5 bg-gradient-to-r from-gray via-primary to-gray "></span>
-              </div>
-            </div>
-            <div className="mt-10 group flex flex-col w-full cursor-pointer">
-              <h2 className="text-[30px]  ">Web Developper - Intern</h2>
-              <div className="flex justify-between font-medium text-[20px] ">
-                <h3>
-                  <span className=" no-underline u group-hover:underline font-medium hover:font-bold  duration-500">
-                    SEED Soft Engine
-                  </span>
-                  , Yaoundé Cameroon
-                </h3>{" "}
-                <h3 className="text-[17px] ">Jully 2023 - September 2023</h3>
-              </div>
-              <h3 className="font-medium text-[15px] mt-5">
-                ReactJS - NodeJS - Express - MongoDB
-              </h3>
-              <div className="flex justify-center">
-                <span className="w-full show-border mt-5 h-0.5 bg-gradient-to-r from-gray via-primary to-gray "></span>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
